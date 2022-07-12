2579a884523f299b3d3cee686e9c4f8ab5e72845
Raul Garcia <raul.garcia@sellbie.com.br>
2022-07-12 09:25:01 -0300

    Base page and styles created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import styles from 'App.module.css';
+import {PlusCircle} from 'phosphor-react';
+
+import styles from './App.module.css';
 import { Header } from './components/Header';
+
+import clipboard from './assets/clipboard.svg';
 
 import './global.css';
 
@@ -8,8 +12,27 @@ function App() {
   return (
     <>
       <Header />
-      <main>
-        
+      <main className={styles.wrapper}>
+        <form action="submit" className={styles.form}>
+          <input placeholder='Adicione uma nova tarefa'/>
+          <button type='submit'>
+            Criar
+            <PlusCircle />
+          </button>
+        </form>
+
+        <div className={styles.listContainer}>
+          <div className={styles.listHeader}>
+            <strong>Tarefas criadas <span>0</span></strong>
+            <strong>Concluídas <span>0</span></strong>
+          </div>
+
+          <div className={styles.listContent}>
+            <img src={clipboard} alt="Lista de tarefas vazia" />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>Cria tarefas e organize seus itens a fazer</p>
+          </div>
+        </div>
       </main>
     </>
   )
